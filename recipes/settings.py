bfe051c0e2b3be6c55ea6c9095bb8bc76293df7d
Ashok <gaireashok36@gmail.com>
2023-08-02 00:06:21 +0545

    By Ashok

--- recipes/settings.py
+++ recipes/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+ucukl*9$7r_x3ez9g3(*9g+-(hejudey+-^8w!m8^$&n2jzi!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
